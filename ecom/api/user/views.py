b87f73162f203a3ce5afcd293d9bfd4fff1aaa4a
Rahul k <48743057+RandyARk79@users.noreply.github.com>
2021-01-23 16:29:00 +0530

    6.1 commit

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login,logout
+import re
+import random
 # Create your views here.
+
+
+def generate_session_token(lenght = 10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)]+[str(i) for i in range(10)]) for _ in range(lenght))
+
+def singin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'send a post request'})
